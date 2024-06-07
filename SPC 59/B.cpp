778390221489ac4fddd54d295df94917e64bdd1d
Ahmadul Hasan <49359716+2018331109@users.noreply.github.com>
2024-06-07 23:53:43 +0600

    fun

---
+++ SPC 59/B.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int x;
+        cin>>x;
+        cout<<x/2<<endl;
+    }
+    return 0;
+}
+
